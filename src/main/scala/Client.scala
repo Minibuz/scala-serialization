be76b1be4daf72bdb4b996d1c4c3cc0a3d43d8ea
Etienne ALEXANDRE <tietienne77@gmail.com>
2022-10-18 22:30:34 +0200

    feat: Create Stub

--- src/main/scala/Client.scala
+++ src/main/scala/Client.scala
@@ -11,18 +11,38 @@ import java.io.PrintWriter
 import java.net.ServerSocket
 import java.net.Socket
 
+class Stub(clientSocket : Socket, out : PrintWriter, in : BufferedReader) extends OperationService:
+  override def add(a: Int, b: Int): Option[Int] = {
+    val request = OperationRequest.serde.serialize(OperationRequest(operator = Operator.ADD, a = a, b = b)) // Stub - Serializing request
+    sendRequest(request)
+  }
+
+  override def minus(a: Int, b: Int): Option[Int] = {
+    val request = OperationRequest.serde.serialize(OperationRequest(operator = Operator.MINUS, a = a, b = b)) // Stub - Serializing request
+    sendRequest(request)
+  }
+
+  override def multiply(a: Int, b: Int): Option[Int] = {
+    val request = OperationRequest.serde.serialize(OperationRequest(operator = Operator.MULTIPLY, a = a, b = b)) // Stub - Serializing request
+    sendRequest(request)
+  }
+  override def divide(a: Int, b: Int): Option[Int] = {
+    val request = OperationRequest.serde.serialize(OperationRequest(operator = Operator.DIVIDE, a = a, b = b)) // Stub - Serializing request
+    sendRequest(request)
+  }
+
+  def sendRequest(request : String): Option[Int] = {
+    out.println(request) // Stub - Sending request to server
+    val resp = in.readLine() // Response from server
+    val response = OperationResponse.serde.deserialize(resp)
+    response.get._1.result
+  }
+
 @main
 def launchClient(): Unit = {
   val clientSocket = Socket("127.0.0.1", 6666)
-  val out = new PrintWriter(clientSocket.getOutputStream, true)
-  val in = new BufferedReader(InputStreamReader(clientSocket.getInputStream))
+  val out = PrintWriter(clientSocket.getOutputStream, true)
+  val in = BufferedReader(InputStreamReader(clientSocket.getInputStream))
 
-  // TODO : Client call Stub
-
-  val request = OperationRequest.serde.serialize(OperationRequest(operator = Operator.ADD, a = 1, b = 1)) // Stub - Serializing request
-  out.println(request) // Stub - Sending request to server
-
-  val resp = in.readLine() // Response from server
-  val response = OperationResponse.serde.deserialize(resp)
-  println(response.get._1.result.get)
+  println(Stub(clientSocket, out, in).add(1, 1).get)
 }
