9f20e3023f547ea77a2fa029d6bdb4e5c67c9fdc
Etienne ALEXANDRE <tietienne77@gmail.com>
2022-10-18 22:14:51 +0200

    feat: possibility to send data between client and server

--- src/main/scala/Client.scala
+++ src/main/scala/Client.scala
@@ -1,7 +1,26 @@
 import scala.util.{Failure, Success, Try}
+import java.net._
+import java.io._
+
+import java.io.BufferedReader
+import java.io.PrintWriter
+import java.net.ServerSocket
+
+import java.io.BufferedReader
+import java.io.PrintWriter
+import java.net.ServerSocket
+import java.net.Socket
 
 @main
 def launchClient(): Unit = {
+  val clientSocket = Socket("127.0.0.1", 6666)
+  val out = new PrintWriter(clientSocket.getOutputStream, true)
+  val in = new BufferedReader(InputStreamReader(clientSocket.getInputStream))
+  out.println("hello server")
+  val resp = in.readLine()
+  println(resp)
+
+  /*
   // TODO : Client call Stub
   val request = OperationRequest.serde.serialize(OperationRequest(operator = Operator.ADD, a = 1, b = 1)) // Stub
   // TODO : Stub send request to server
@@ -18,4 +37,5 @@ def launchClient(): Unit = {
       // TODO : Server send response to Stub
     case Failure(e) => println(e)
   }
+  */
 }
--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -1,6 +1,29 @@
 import scala.util.{Failure, Success, Try}
+import java.net._
+import java.io._
 
+import java.io.BufferedReader
+import java.io.PrintWriter
+import java.net.ServerSocket
+
+import java.io.BufferedReader
+import java.io.PrintWriter
+import java.net.ServerSocket
+import java.net.Socket
+
+def stop(in: BufferedReader, out : BufferedWriter, clientSocket : Socket, serverSocket: ServerSocket): Unit = {
+  in.close()
+  out.close()
+  clientSocket.close()
+  serverSocket.close()
+}
 @main
 def launchServer(): Unit = {
-
+  val serverSocket = ServerSocket(6666)
+  val clientSocket = serverSocket.accept
+  val out = new PrintWriter(clientSocket.getOutputStream, true)
+  val in = new BufferedReader(InputStreamReader(clientSocket.getInputStream))
+  val greeting = in.readLine
+  if ("hello server" == greeting) out.println("hello client")
+  else out.println("unrecognised greeting")
 }
