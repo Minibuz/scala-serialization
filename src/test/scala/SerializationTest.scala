bb52156fd2bec67d0151cd4a3c177f586149619b
LAGINHA Guillaume <darkanos00@gmail.com>
2022-10-20 16:30:11 +0200

    Add Test with exception

--- src/test/scala/SerializationTest.scala
+++ src/test/scala/SerializationTest.scala
@@ -1,6 +1,7 @@
 import org.scalatest.funsuite.AnyFunSuite
 
 import scala.util.Success
+import scala.util.Failure
 
 class SerializationTest extends AnyFunSuite {
 
@@ -18,6 +19,18 @@ class SerializationTest extends AnyFunSuite {
     assert(int == Success(9999, Input(strForInt, 11)))
   }
 
+  test("Deserialization for INT with less with less than 11 characters") {
+    val strForInt = "     9"
+    val int = PseudobinSerde.INT.deserialize(strForInt)
+    assertThrows[StringIndexOutOfBoundsException](int.get)
+  }
+
+  test("Deserialization for INT with letter") {
+    val strForInt = "          D"
+    val int = PseudobinSerde.INT.deserialize(strForInt)
+    assertThrows[NumberFormatException](int.get)
+  }
+
   test("Serialization and Deserialization for SHORT = 1") {
     val strForShort = PseudobinSerde.SHORT.serialize(1)
     assert(strForShort == "     1")
@@ -30,6 +43,18 @@ class SerializationTest extends AnyFunSuite {
     assert(strForShort == "  9999")
     val short = PseudobinSerde.SHORT.deserialize(strForShort)
     assert(short == Success(9999, Input(strForShort, 6)))
+  }
+
+  test("Deserialization for SHORT with less with less than 6 characters") {
+    val strForShort = "   9"
+    val short = PseudobinSerde.SHORT.deserialize(strForShort)
+    assertThrows[StringIndexOutOfBoundsException](short.get)
+  }
+
+  test("Deserialization for SHORT with letter") {
+    val strForShort = "     D"
+    val short = PseudobinSerde.SHORT.deserialize(strForShort)
+    assertThrows[NumberFormatException](short.get)
   }
 
   test("Serialization and Deserialization for LONG = 1") {
@@ -46,6 +71,18 @@ class SerializationTest extends AnyFunSuite {
     assert(long == Success(9999, Input(strForLong, 20)))
   }
 
+  test("Deserialization for LONG with less with less than 20 characters") {
+    val strForLONG = "          58"
+    val long = PseudobinSerde.LONG.deserialize(strForLONG)
+    assertThrows[StringIndexOutOfBoundsException](long.get)
+  }
+
+  test("Deserialization for LONG with letter") {
+    val strForLONG = "                  AZ"
+    val long = PseudobinSerde.LONG.deserialize(strForLONG)
+    assertThrows[NumberFormatException](long.get)
+  }
+
   test("Serialization and Deserialization for DOUBLE = 1.1") {
     val strForDouble = PseudobinSerde.DOUBLE.serialize(1.1)
     assert(strForDouble == "                     1.1")
@@ -60,6 +97,18 @@ class SerializationTest extends AnyFunSuite {
     assert(double == Success(-1e1, Input(strForDouble, 24)))
   }
 
+  test("Deserialization for DOUBLE with less with less than 24 characters") {
+    val strForDOUBLE = "                  85"
+    val double = PseudobinSerde.DOUBLE.deserialize(strForDOUBLE)
+    assertThrows[StringIndexOutOfBoundsException](double.get)
+  }
+
+  test("Deserialization for DOUBLE with letter") {
+    val strForDOUBLE = "                      TOTO"
+    val double = PseudobinSerde.DOUBLE.deserialize(strForDOUBLE)
+    assertThrows[NumberFormatException](double.get)
+  }
+
   test("Serialization and Deserialization for BOOLEAN = true") {
     val strForBoolean = PseudobinSerde.BOOLEAN.serialize(true)
     assert(strForBoolean == " true")
@@ -72,6 +121,12 @@ class SerializationTest extends AnyFunSuite {
     assert(strForBoolean == "false")
     val boolean = PseudobinSerde.BOOLEAN.deserialize(strForBoolean)
     assert(boolean == Success(false, Input(strForBoolean, 5)))
+  }
+
+  test("Deserialization for BOOLEAN with invalid String") {
+    val strForBoolean = "TRUEE"
+    val boolean = PseudobinSerde.BOOLEAN.deserialize(strForBoolean)
+    assertThrows[IllegalArgumentException](boolean.get)
   }
 
   test("Serialization and Deserialization for String = \"Test\"") {
